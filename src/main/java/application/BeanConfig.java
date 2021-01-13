e5b070a4aee22ee500d99378e4733bcea4ee8d07
valor4valkyrie <c.george.pemberton@gmail.com>
2021-01-13 16:17:42 -0600

    Removed 'PropertySource' and the specific component scan.

--- src/main/java/application/BeanConfig.java
+++ src/main/java/application/BeanConfig.java
@@ -1,19 +1,12 @@
 package application;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
 
 @Configuration
-@PropertySource(value = "classpath:application.properties")
-@ComponentScan(basePackages = {"application.stats", "v1"})
+@ComponentScan
 public class BeanConfig {
-
-    @Autowired
-    Environment env;
 
     @Bean
     public ApplicationContextProvider applicationContextProvider() {
